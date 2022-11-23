56d7a15bb475e7b4c31171c9f1fde7ff4679ab48
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-23 22:18:39 +0530

    Solution added

---
+++ MoodAnalyzerTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyserProblem;
+
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //UC1
+        //Display Sad message
+        //TC 1.1
+        [TestMethod]
+        public void GivenSadMoodMessage_WhenAnalyse_ShouldReturnSAD()
+        {
+
+            string message = "I am sad mood";
+            string expectedValue = "SAD";
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+            string result = moodAnalyser.AnalyseMood(message);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+        //Display Happy message
+        //TC 1.2
+        [TestMethod]
+        public void GivenSadMoodMessage_WhenAnalyse_ShouldReturnHAPPY()
+        {
+
+            string message = "I am happy mood";
+            string expectedValue = "HAPPY";
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string result = moodAnalyser.AnalyseMood(message);
+            Assert.AreEqual(expectedValue, result);
+        }
+
+    }
+}
